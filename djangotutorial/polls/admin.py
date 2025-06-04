f104650dda3208e06249afb04a5445f095c4ef79
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2025-06-04 12:09:20 +0300

    choices implemented in creating new question

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -3,5 +3,22 @@ from .models import Question, Choice
 
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # Changing form fields order
+    """ fields = ["pub_date", "question_text"] """
+
+    # Using fieldsets
+    fieldsets = [
+        ("Define Question", {"fields": ["question_text"]}), 
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ] 
+    inlines =[ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+
+# Separate registration of Choice
+""" admin.site.register(Choice) """
